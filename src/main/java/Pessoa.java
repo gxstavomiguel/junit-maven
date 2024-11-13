b209f46904c3be5a18d6d21a036068ac3ab98da8
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-12 21:23:51 -0300

    Instanciando a classe Pessoa e realizando testes

--- src/main/java/Pessoa.java
+++ src/main/java/Pessoa.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Pessoa {
+    private String nome;
+
+    private LocalDateTime nascimento;
+
+    public Pessoa(String nome, LocalDateTime nascimento){
+        this.nome = nome;
+        this.nascimento = nascimento;
+    }
+
+    public int getIdade(){
+        return (int) ChronoUnit.YEARS.between(nascimento, LocalDateTime.now());
+    }
+
+    public boolean ehMaiorDeIdade(){
+        return getIdade() >= 18;
+    }
 }
